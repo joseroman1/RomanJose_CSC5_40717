8cb27cc7df94e44b9bbff61d6508f25e5d014967
jr2436454 <jroman22@student.rcc.edu>
2015-01-07 11:30:55 -0800

    Signed-off-by: jr2436454 <jroman22@student.rcc.edu>

---
+++ Class-Lab/MathInstructorDisagreement/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Roman
+ * Created on January 7, 2015, 11:17 AM
+ *      Purpose: How easy computers can make mistakes
+*/
+#include <iostream>
+
+using namespace std;
+
+// User Libraries
+ 
+//Global Constants
+
+//Functions Prototypes
+
+//Executions begin here!
+int main(int argc, char** argv) {
+    //Declare 3 variables
+    short a,b,c;
+    //Prompt for a and b
+    cout<<"Input 2 short values"<<endl;
+    cin>>a,b;
+    //Sum the values
+
+    return 0;
+}
+
